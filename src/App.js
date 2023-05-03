413d5cd82f5e03f90d032a8bc49218ac5d9afbe3
OhHyukJun <ohj3961@gmail.com>
2023-05-03 14:59:30 +0900

    Feat: subject2-1

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {BrowserRouter as  Router,Routes, Route } from "react-router-dom";
+import  NavBar  from './components/NavBar';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import './App.css';
+
+function App(){
+  return (
+    <React.Fragment>
+      <NavBar/>
+          <React.Fragment>
+            <Router>
+            <Routes>
+            <Route exact path="/" element={<Home/>}/>  
+            </Routes>
+            </Router>
+          </React.Fragment>
+      <Footer/>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import '../App.css';
+import React from 'react';
+
+function Home(){
+  return (
+    <React.Fragment className="p-5">
+      <div className="main-bg"></div>
+    </React.Fragment>
+  );
+}
+
+export default Home;
