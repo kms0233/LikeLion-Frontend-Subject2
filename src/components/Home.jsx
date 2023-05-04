1e6d0c9d76cbc7191701457e8a600c38bdc99739
kimchchchchchche <codus618@naver.com>
2023-05-04 19:29:05 +0900

    Feat:과제2-1 제출

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import '../App.css';
+import React from 'react';
+
+function Home() {
+  return (
+    <React.Fragment>
+      <div className="main-bg" />
+     
+    </React.Fragment>
+    
+  );
+}
+
+export default Home;
