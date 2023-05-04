4ca5456fb0d078db4aacd2c027651a5a68577744
seoyeon-jung <jungluchia1204@gmail.com>
2023-05-04 19:48:13 +0900

    feat: albums 추가
    - album 이미지와 설명을 추가했습니다

--- src/pages/Albums.jsx
+++ src/pages/Albums.jsx
@@ -1,5 +1,53 @@
+import { Container, Row, Col } from "react-bootstrap";
+import "../App.css";
+
 const Albums = () => {
-  return <>Hello</>;
+  const Lilac = require("../img/Lilac.jpg");
+  const LovePoem = require("../img/LovePoem.jpg");
+  const Palette = require("../img/Palette.jpg");
+
+  return (
+    <Container className="p-5">
+      <Row>
+        <Col className="m-3">
+          <Row>
+            <img
+              className="col-4"
+              style={{ width: "400px" }}
+              src={LovePoem}
+              alt=""
+            />
+            <div className="album-title">Love peom</div>
+            <div> The 5th Mini Album</div>
+          </Row>
+        </Col>
+        <Col className="m-3">
+          <Row>
+            <img
+              className="col-4"
+              style={{ width: "400px" }}
+              src={Lilac}
+              alt=""
+            />
+            <div className="album-title">Lilac</div>
+            <div> The 5th Album</div>
+          </Row>
+        </Col>
+        <Col className="m-3">
+          <Row>
+            <img
+              className="col-4"
+              style={{ width: "400px" }}
+              src={Palette}
+              alt=""
+            />
+            <div className="album-title">Palette</div>
+            <div> The 4th Album</div>
+          </Row>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Albums;
