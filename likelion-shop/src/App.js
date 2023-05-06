3a2fe95625e4d855338651b1bd3231976e06d5e0
subin <subin.kk019@gmail.com>
2023-05-06 22:57:16 +0900

    Feat: router 설정

--- likelion-shop/src/App.js
+++ likelion-shop/src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./styles/App.css";
-import Footer from "./components/footer";
+import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div>
       <NavBar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </BrowserRouter>
       <Footer />
     </div>
   );
--- likelion-shop/src/components/NavBar.js
+++ likelion-shop/src/components/NavBar.js
@@ -4,7 +4,7 @@ import { Container, Navbar } from "react-bootstrap";
 
 function NavBar() {
   return (
-    <Navbar>
+    <Navbar bg="light" expand="lg">
       <Container>
         <Navbar.Brand href="#home">멋사 shop</Navbar.Brand>
       </Container>
