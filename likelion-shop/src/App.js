cc0c97365a28a10f9b0523b9fcd25a23f3e90c86
subin <subin.kk019@gmail.com>
2023-05-07 00:00:58 +0900

    Fix: 잘못 추가된 코드 수정

--- likelion-shop/src/App.js
+++ likelion-shop/src/App.js
@@ -15,20 +15,6 @@ function App() {
             <Route path="/" element={<Home />}></Route>
           </Routes>
         </BrowserRouter>
-        <li>리스트</li>
-        <li>리스트</li>
-        <li>리스트</li>
-        <li>리스트</li>
-        <li>리스트</li>
-        <li>리스트</li>
-        <li>리스트</li>
-        <li>리스트</li>
-        <li>리스트</li>
-        <li>리스트</li>
-        <li>리스트</li>
-        <li>리스트</li>
-        <li>리스트</li>
-        <li>리스트</li>
       </div>
       <Footer />
     </div>
