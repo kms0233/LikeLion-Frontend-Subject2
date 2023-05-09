9a7844dafed464faceba67bb9287e3339343cd40
subin <subin.kk019@gmail.com>
2023-05-09 15:09:02 +0900

    Feat: album 제목 및 설명 추가

--- likelion-shop/src/components/Albums.js
+++ likelion-shop/src/components/Albums.js
@@ -9,20 +9,20 @@ function Albums() {
   return (
     <Container>
       <Row>
-        <Col>
-          <img src={lilac} className="album-img" alt="lilac-img"></img>
-          <h5>Lilac</h5>
-          <p>The 5th Mini Album</p>
+        <Col md>
+          <img src={lilac} className="albumImg" alt="lilac-img"></img>
+          <div className="albumTitle">Lilac</div>
+          <div className="albumCaption">The 5th Mini Album</div>
         </Col>
-        <Col>
-          <img src={lovepoem} className="album-img" alt="lovepoem-img"></img>
-          <h5>Love Poem</h5>
-          <p>The 5th Album</p>
+        <Col md>
+          <img src={lovepoem} className="albumImg" alt="lovepoem-img"></img>
+          <div className="albumTitle">Love Poem</div>
+          <div className="albumCaption">The 5th Album</div>
         </Col>
-        <Col>
-          <img src={palette} className="album-img" alt="palette-img"></img>
-          <h5>Palette</h5>
-          <p>The 4th Album</p>
+        <Col md>
+          <img src={palette} className="albumImg" alt="palette-img"></img>
+          <div className="albumTitle">Palette</div>
+          <div className="albumCaption">The 4th Album</div>
         </Col>
       </Row>
     </Container>
