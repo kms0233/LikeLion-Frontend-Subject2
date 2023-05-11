a98f34b274ea5ebefdd41a7784409b31de8c45e8
unknown <jukw3135@naver.com>
2023-05-11 19:31:40 +0900

    Docs: 과제 2-2 수정 및 재제출

--- homepage/src/pages/album.jsx
+++ homepage/src/pages/album.jsx
@@ -11,40 +11,23 @@ function Album() {
       <Row>
         <Col md={4}>
           <img src={Firstimg} alt="Firstimg" className="imgsize" />
-        </Col>
-        <Col md={4}>
-          <img src={Secondimg} alt="Secondimg" className="imgsize" />
-        </Col>
-        <Col md={4}>
-          <img src={Thirdimg} alt="Thirdimg" className="imgsize" />
-        </Col>
-      </Row>
-
-      <Row>
-        <Col md={4}>
           <h5 className="text">
             <b>Love poem</b>
           </h5>
-        </Col>
-        <Col md={4}>
-          <h5 className="text">
-            <b>LILAC</b>
-          </h5>
-        </Col>
-        <Col md={4}>
-          <h5 className="text">
-            <b>Pelette</b>
-          </h5>
-        </Col>
-      </Row>
-      <Row>
-        <Col md={4}>
           <h6 className="text">The 5th Mini Album</h6>
         </Col>
         <Col md={4}>
+          <img src={Secondimg} alt="Secondimg" className="imgsize" />
+          <h5 className="text">
+            <b>LILAC</b>
+          </h5>
           <h6 className="text">The 5th Album</h6>
         </Col>
         <Col md={4}>
+          <img src={Thirdimg} alt="Thirdimg" className="imgsize" />
+          <h5 className="text">
+            <b>Pelette</b>
+          </h5>
           <h6 className="text">The 4th Album</h6>
         </Col>
       </Row>
