4f9a4e64e16e94b3a831c41fe0c89bfd4801832c
kms0233 <cnblue789@naver.com>
2023-05-31 16:35:38 +0900

    Feat: Subject2_3

--- src/AlbumDetail.js
+++ src/AlbumDetail.js
@@ -7,6 +7,7 @@ import "./AlbumDetail.css";
 function AlbumDetail(props) {
   const { title } = useParams();
   const [showSaleAlert, setShowSaleAlert] = useState(true);
+  const [currentPrice, setCurrentPrice] = useState(0);
 
   const album = data.find((album) => album.title === title);
   const imagePath = `${process.env.PUBLIC_URL}/images/${album.image}`;
@@ -14,37 +15,41 @@ function AlbumDetail(props) {
   useEffect(() => {
     const timeout = setTimeout(() => {
       setShowSaleAlert(false);
+      setCurrentPrice(20000);
     }, 3000);
     return () => clearTimeout(timeout);
   }, []);
 
+  useEffect(() => {
+    setCurrentPrice(14000);
+  }, [album]);
+
   return (
-    <div className="container" >
+    <div className="container">
       {showSaleAlert && (
         <Alert
-          variant="success"
-          style={{ backgroundColor: "#FFF3CD", height: "50px",border:"0px", marginTop:"15px"}}
+          variant="primary"
           onClose={() => setShowSaleAlert(false)}
           dismissible={false}
+          style={{ height: "50px" ,marginTop:"10px"}}
         >
-        <p style={{color: "#6E560E", margin: "auto", fontWeight:"bold",fontSize:"14px"}}>3초 안에 구매하면 30% 세일 중입니다!</p>
-      </Alert>
-)}
+          <Alert.Heading style={{ fontSize: "14px", fontWeight: "bold", margin: 0 }}>
+            3초 안에 구매하면 30% 세일 중입니다!
+          </Alert.Heading>
+        </Alert>
+      )}
 
       <div className="row">
         <div className="col-4 d-flex">
           <div className="image-container d-flex align-items-center">
-            <img src={imagePath} alt="Album" width="600px"/>
+            <img src={imagePath} alt="Album" width="600px" />
           </div>
         </div>
         <div className="col-8 d-flex flex-column">
           <h2 style={{ marginTop: "100px" }}>{album.title}</h2>
           <h4>{album.content}</h4>
-          <h6>{album.price}</h6>
-          <button
-            className="button"
-            style={{ marginLeft: "200px" }}
-          >
+          <h6>{currentPrice}원</h6>
+          <button className="btn btn-primary" style={{ width: "100px", marginLeft: "200px" }}>
             주문하기
           </button>
         </div>
